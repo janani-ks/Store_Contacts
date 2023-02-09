94f9fc2a73d57b15d596a52c03bd05699cb45d3c
janani-ks <123439325+janani-ks@users.noreply.github.com>
2023-02-09 18:09:05 +0530

    Update Contacts.java

--- simple/src/main/java/contacts/Contacts.java
+++ simple/src/main/java/contacts/Contacts.java
@@ -2,170 +2,277 @@ package contacts;
 import java.io.FileDescriptor;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.Scanner;
-class StoreAndManage{
+class Details{
 	static PrintStream p=new PrintStream((new FileOutputStream(FileDescriptor.out)));
 	String name;
-	long number;
+	String number;
 	String email;
-	StoreAndManage(String n,long n1,String e){
+	Details(String n,String n1,String e){
 		this.name = n;
 		this.number = n1;
 		this.email = e;
 	}
 }
-public class Contacts {
+class LinkedList{
+	public int c;
+	class Node{
+		Details data;
+		Node next;
+		public Node(Details d) {
+			this.data = d;
+			this.next = null;
+		}
+	}
+	
+	public Node root = null;
+	public Node tail = null;
+	public void addObject(Details d) {
+		Node newnode = new Node(d);
+		if(root == null) {
+			root = newnode;
+			tail = newnode;
+		}
+		else {
+			tail.next = newnode;
+			tail = newnode;
+		}
+	}
+	public boolean removeByName(String n) {
+		Node temp = root;
+		Node pre = temp;
+		if(root == null) {
+			return false;
+		}
+	    else if(root.data.name.equals(n)) {
+	    	root = root.next;
+	    	return false;
+	    }
+		else {
+		while(temp != null  ) {
+			if(temp.data.name.equals(n)) {
+				pre.next = temp.next;
+				return false; 
+			}
+			pre = temp;
+			temp = temp.next;
+		}
+		return true;
+		}		
+	}
+	public boolean removeByNumber(String n) {
+		Node temp = root;
+		Node pre = temp;
+		if(root == null) {
+			return false;
+		}
+	    else if(root.data.number == n) {
+	    	root = root.next;
+	    	return false;
+	    }
+		else {
+		while(temp != null  ) {
+			if(temp.data.number.equals(n)) {
+				pre.next = temp.next;
+				return false; 
+			}
+			pre = temp;
+			temp = temp.next;
+		}
+		return true;
+		}		
+	}
+	public boolean removeByEmail(String n) {
+		Node temp = root;
+		Node pre = temp;
+		if(root == null) {
+			return false;
+		}
+	    else if(root.data.email.equals(n)) {
+	    	root = root.next;
+	    	return false;
+	    }
+		else {
+		while(temp != null  ) {
+			if(temp.data.email.equals(n)) {
+				pre.next = temp.next;
+				return false;  
+			}
+			pre = temp;
+			temp = temp.next;
+		}
+		return true;
+		}		
+	}	
+    public Details searchByName(String n) {
+    	c = 0;
+    	Node temp = root;
+		if(root == null) {
+			return null;
+		}
+	    else if(root.data.name.equals(n)) {
+	    	return root.data;
+	    }
+		else {
+		while(temp != null  ) {
+			if(temp.data.name.equals(n)) {
+				return temp.data;  
+			}
+			c++;
+			temp = temp.next;
+		}
+		return null;
+		}		
+	}	
+    public Details searchByNumber(String n) {
+    	c = 0;
+    	Node temp = root;
+		if(root == null) {
+			return null;
+		}
+	    else if(root.data.number.equals(n)) {
+	    	return root.data;
+	    }
+		else {
+		while(temp != null  ) {
+			if(temp.data.number.equals(n)){
+				return temp.data;  
+			}
+			temp = temp.next;
+			c++;
+		}
+		return null;
+		}		
+	}
+    public Details searchByEmail(String n) {
+    	c = 0;
+    	Node temp = root;
+		if(root == null) {
+			return null;
+		}
+	    else if(root.data.email.equals(n)) {
+	    	return root.data;
+	    }
+		else {
+		while(temp != null  ) {
+			if(temp.data.email.equals(n)) {
+				return temp.data;  
+			}
+			temp = temp.next;
+			c++;
+		}
+		return null;
+		}		
+	}
+    public void printList(){
+    	int c1=1;
+		if(root == null) {
+			Details.p.println("List is Empty");
+		}
+		else {
+			Node temp = root;
+			while(temp!=null) {
+				Details.p.println("Person "+c1+" Details !!");
+				Details.p.println("Name = "+temp.data.name);
+				Details.p.println("Number = "+temp.data.number);
+				Details.p.println("Email Address = "+temp.data.email);
+				Details.p.println("\n");
+				c1++;
+				temp = temp.next;
+			}
+		}
+	}
+}
+public class Storecontacts {
+	static int choice;
+	static int count = 1 ;
+	static long number1;
+	static String number;
+	static String name;
+	static String email;
+	static boolean b ;
 	static Scanner s = new Scanner(System.in);
 	static Scanner s1 = new Scanner(System.in);
-	static Scanner s2 = new Scanner(System.in);
-	static ArrayList<StoreAndManage> list = new ArrayList<StoreAndManage>();
-	static String name;
-	static long number;
-	static String email;
-	static int count=1;
-	static int c;
-	static boolean b;
-	static int choice;
-	static void add() {
-		StoreAndManage.p.println("Enter Person "+count+" Details!!");
-		StoreAndManage.p.print("Enter Name = ");
-		name = s.nextLine();
-		StoreAndManage.p.print("Enter Contact Number = ");
-		number = s1.nextLong();
-		StoreAndManage.p.print("Enter Email Address = ");
-		email = s2.nextLine();	
-		list.add(new StoreAndManage(name,number,email));
-		count++;
-	}
-    static StoreAndManage searchByName(String n) {
-    	c =0;
-    	b = true;
-    	for(StoreAndManage i: list) {
-			if(i.name.equals(n)) {
-				b = false;
-				return i;
-			}
-			c++;
-		}
-    	return null;
-    }
-    static StoreAndManage searchByNumber(long n) {
-    	c =0;
-    	b = true;
-    	for(StoreAndManage i: list) {
-			if(i.number == n) {
-				b = false;
-				return i;
-			}
-			c++;
-		}
-    	return null;
-    }
-    static StoreAndManage searchByEmail(String n) {
-    	c =0;
-    	b = true;
-    	for(StoreAndManage i: list) {
-			if(i.email.equals(n)) {
-				b = false;
-				return i;
-			}
-			c++;
-		}
-    	return null;
-    }
+    static Scanner s2 = new Scanner(System.in);
+    static Scanner s3 = new Scanner(System.in);
+    static LinkedList l = new LinkedList();
+    static Details k ;
     static void store() {
-    	StoreAndManage.p.println("Enter the Choice like 1,2,3..7 What You Want !!\n1. Add Details \n2. Delete by Name \n3. Delete By Number\n4. Delete By Email\n5. Search By Name\n6. Search By Number\n7. Search By Email" );
-		choice = s1.nextInt();
-		StoreAndManage k = null;
-		switch(choice) {
-		      case 1:
-		    	  add();
-		    	  break;
-		      case 2:
-		    	  StoreAndManage.p.println("Enter the Name");
-		    	  String n = s.nextLine();
-		    	  b = true;
-		  		  for(int i=0;i<list.size();i++) {
-		    			if(list.get(i).name.equals(n)) {
-		    				list.remove(i);
-		    				b = false;
-		    			}
-		    	  }
-		    	  break;
-		      case 3:
-		    	  StoreAndManage.p.println("Enter the Number");
-		    	  long n1 = s1.nextInt();
-		    	  b = true;
-		      	  for(int i=0;i<list.size();i++) {
-		    			if(list.get(i).number == n1) {
-		    				list.remove(i);
-		    				b = false;
-		    			}
-		    	  }
-		      	  break;
-		      case 4:
-		    	  StoreAndManage.p.println("Enter the Email Address");
-		    	  String n3 = s2.nextLine();
-		    	  b = true;
-		      	  for(int i=0;i<list.size();i++) {
-		    			if(list.get(i).email.equals(n3)) {
-		    				list.remove(i);
-		    				b = false;
-		    			}
-		      	  }
-		    	  break;
-		      case 5:
-		    	  StoreAndManage.p.println("Enter the Name");
-		    	  String n4 = s.nextLine();
-		    	  k = searchByName(n4);
-		    	  break;
-		      case 6:
-		    	  StoreAndManage.p.println("Enter the Number");
-		    	  long n5 = s1.nextInt();
-		    	  k = searchByNumber(n5);
-		    	  break;
-		      case 7:
-		    	  StoreAndManage.p.println("Enter the Email Adress");
-		    	  String n6 = s2.nextLine();
-		    	  k = searchByEmail(n6);
-		    	  break;
-		      default:
-		    	  StoreAndManage.p.println("Enter the Correct Choice !!");
-		    	  break;
-		    	 
-		}
-		if(b && (choice>=2 &&choice<=7)) {
-	      	  StoreAndManage.p.println("There is no Details of the person");
-	    }
-		else if(b && choice>=5 &&choice<=7) {
-			StoreAndManage.p.println("The index of the searched person is "+c);
-			StoreAndManage.p.println("Name = "+k.name);
-			StoreAndManage.p.println("Number = "+k.number);
-			StoreAndManage.p.println("Email Address = "+k.email);
-		}
-    }
-	public static void main(String[] arg) {
-		add();
-		add();
-		add();
-		char str;
+    k = null;
+    b = true;
+    Details.p.println("Enter the Choice like 1,2,3..7 What You Want !!\n1. Add Details \n2. Delete by Name \n3. Delete By Number\n4. Delete By Email\n5. Search By Name\n6. Search By Number\n7. Search By Email" );
+	choice = s1.nextInt();
+	switch(choice){
+	      case 1:
+	    	Details.p.println("Enter Person "+count+" Details!!");
+	  		Details.p.print("Enter Name = ");
+	  		name = s.nextLine();
+	  		Details.p.print("Enter Contact Number = ");
+	  		number = s3.nextLine();
+	  		number1 = Long.valueOf(number);
+	  		if(number1 <= 1000000000l || number1 >=10000000000l) {
+	  			Details.p.print("Enter correct 10 digit Contact Number = ");
+	  			number = s3.nextLine();
+	  		}
+	  		Details.p.print("Enter Email Address = ");
+	  		email = s2.nextLine();	
+	  		l.addObject((new Details(name,number,email)));
+	  		count++;
+	    	break;
+	      case 2:
+	    	  Details.p.println("Enter the Name");
+	    	  String n = s.nextLine();
+	    	  b = l.removeByName(n);
+	    	  break;
+	      case 3:
+	    	  Details.p.println("Enter the Number");
+	    	  String n1 = s3.nextLine();
+	    	  b = l.removeByNumber(n1);
+	      	  break;
+	      case 4:
+	    	  Details.p.println("Enter the Email Address");
+	    	  String n3 = s2.nextLine();
+	    	  b = l.removeByEmail(n3);
+	    	  break;
+	      case 5:
+	    	  Details.p.println("Enter the Name");
+	    	  String n4 = s.nextLine();
+	    	  k = l.searchByName(n4);
+	    	  break;
+	      case 6:
+	    	  Details.p.println("Enter the Number");
+	    	  String n5 = s3.nextLine();
+	    	  k = l.searchByNumber(n5);
+	    	  break;
+	      case 7:
+	    	  Details.p.println("Enter the Email Adress");
+	    	  String n6 = s2.nextLine();
+	    	  k = l.searchByEmail(n6);
+	    	  break;
+	      default:
+	    	  Details.p.println("Enter the Correct Choice !!");
+	    	  break;
+	}
+	if(b && k==null && (choice>=2 && choice<=7)){
+		Details.p.println("There is no Details of the person");
+	}
+	else if(choice>=5 && choice<=7) {
+		Details.p.println("The index of the searched person is "+l.c);
+		Details.p.println("Name = "+k.name);
+		Details.p.println("Number = "+k.number);
+		Details.p.println("Email Address = "+k.email);
+	}
+	}
+	public static void main(String[] args) {
+		char str ;
+		store();
 		while(1>0) {
 			store();
-			StoreAndManage.p.println("Do you want to do anyother operation Y/N");
+			Details.p.println("Do you want to do anyother operation Y/N");
 			str = s1.next().charAt(0);
 			if(str == 'N')
 				break;
 		}
-		int j=1;
-		for(StoreAndManage i:list) {
-			StoreAndManage.p.println("Person "+j+" Details !!");
-			StoreAndManage.p.println("Name = "+i.name);
-			StoreAndManage.p.println("Number = "+i.number);
-			StoreAndManage.p.println("Email Address = "+i.email);
-			j++;
-			StoreAndManage.p.println("\n");
-		}
-		
-	}
+		l.printList();
+    }
 }
